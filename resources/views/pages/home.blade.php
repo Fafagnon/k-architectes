5187ac2837283450981690bca8972bbb3f307056
fafagnon <fafagnonadama@gmail.com>
2026-09-23 22:02:59 +0000

    fix(carousel): defilement lineaire continu sans arret et boucle infinie fluide

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -63,18 +63,20 @@
     <div class="swiper swiper-projets" aria-label="Galerie des projets à la une">
       <div class="swiper-wrapper">
         @if(isset($projects) && !empty($projects))
-          @foreach($projects as $slug => $projet)
-            <div class="swiper-slide">
-              <a class="projet-card" href="{{ route('realisations.show', $slug) }}" aria-label="Voir {{ $projet['titre'] }}">
-                <div class="cadre projet-card__photo">
-                  <img src="{{ asset($projet['couverture']) }}" alt="{{ $projet['titre'] }}" loading="lazy" decoding="async">
-                  <div class="projet-card__survol" aria-hidden="true">
-                    <span class="projet-card__badge">En savoir plus <span class="index__fleche">&rarr;</span></span>
+          @for($rep = 0; $rep < 3; $rep++)
+            @foreach($projects as $slug => $projet)
+              <div class="swiper-slide">
+                <a class="projet-card" href="{{ route('realisations.show', $slug) }}" aria-label="Voir {{ $projet['titre'] }}">
+                  <div class="cadre projet-card__photo">
+                    <img src="{{ asset($projet['couverture']) }}" alt="{{ $projet['titre'] }}" loading="lazy" decoding="async">
+                    <div class="projet-card__survol" aria-hidden="true">
+                      <span class="projet-card__badge">En savoir plus <span class="index__fleche">&rarr;</span></span>
+                    </div>
                   </div>
-                </div>
-              </a>
-            </div>
-          @endforeach
+                </a>
+              </div>
+            @endforeach
+          @endfor
         @endif
       </div>
     </div>
